dcebf4fb553f81b7e0d3ee4e72030dab840fed5b
WaelBEHAR <149507482+WaelBEHAR@users.noreply.github.com>
2023-11-02 13:46:46 +0100

    Update branche_wael.c

--- branche_wael.c
+++ branche_wael.c
@@ -1,38 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    int Choix;
-    printf("selectionnez un des choix ci-dessous \n");
-
-    printf("\t1.  Regles du jeu (affichees a l'ecran)\n\t2.  Lancer un nouveau jeu a partir du niveau 1\n\t3.  Charger une partie\n\t4.  Mot de Passe\n\t5.  Scores\n\t6.  Quitter\n");
-    scanf("%d",&Choix);
-
-    if (Choix == 1 ){
-        printf("Regles du jeu:\nChaque niveau devra etre resolu en moins de 120 secondes.\nSi le temps est ecoule, le joueur perd une vie et recommence le niveau.\nLe but est de recuperer les 4 oiseaux du niveau sans se faire toucher par la balle et/ou les ennemis (si presents).\n");
-    }
-    if (Choix == 2){
-        printf("lancement du jeu a partir du niveaux 1");
-    }
-    if (Choix == 3){
-        printf("vous voulez charger quelle partie");
-    }
-    if(Choix == 4){
-        printf("quelle est le mot de passe ?");
-    }
-    if(Choix== 5){
-        printf("votre score est...");
-    }
-    if (Choix== 6){
-        printf("merci pour votre partie ");
-
-    }
-    if(Choix > 6){
-        printf("erreur de saisie selectionnez a nouveau\n");
-        printf("\t1.  Regles du jeu (affichees a l'ecran)\n\t2.  Lancer un nouveau jeu a partir du niveau 1\n\t3.  Charger une partie\n\t4.  Mot de Passe\n\t5.  Scores\n\t6.  Quitter\n");
-        scanf("%d",&Choix);
-    }
-
-
-
-
+for(int i = 0; i < n; i++) {
+  for(int j = 0; j < m; j++)
+    if(m[i][j])
+      printf(" x ");
+    else
+      printf(" 0 ");
+  printf("\n");
 }
